c42535b5902a96f54876c84a209e0d5b549af2be
Хегай Артем <khegay.art@gmail.com>
2021-09-15 19:00:02 +0300

    add tests for List component

--- src/components/List/List.test.tsx
+++ src/components/List/List.test.tsx
@@ -1,17 +1,29 @@
 import List from ".";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { listData } from "TestData/ListData";
 
 describe("List Component", () => {
-  it("Render test with data", () => {
-    render(<List items={listData} />);
-
-    expect(screen.getByRole("list")).toBeInTheDocument();
-  });
-
   it("Render test with no-data", () => {
     render(<List items={[]} />);
 
     expect(screen.queryByRole("list")).toBeNull();
+  });
+
+  it("Render test with empty string data", () => {
+    render(<List items={[""]} />);
+
+    expect(screen.queryByRole("list")).toBeInTheDocument();
+  });
+
+  it("Render test with data", () => {
+    render(<List items={listData} />);
+
+    expect(screen.getByRole("list")).toBeInTheDocument();
+
+    const list = screen.getByRole("list");
+
+    const { getAllByRole } = within(list);
+    const listItems = getAllByRole("listitem");
+    expect(listItems.length).toBe(listItems.length);
   });
 });
