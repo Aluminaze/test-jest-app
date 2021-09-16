383309db244f8bbe1e50a8ac52a6655a34bd74ca
Хегай Артем <khegay.art@gmail.com>
2021-09-16 15:31:54 +0300

    add logic to filter list by search

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./App.module.css";
 import List from "components/List";
 import { listData } from "TestData/ListData";
@@ -6,15 +6,28 @@ import Search from "components/Search";
 
 function App() {
   const [value, setValue] = useState<string>("");
+  const [data, setData] = useState<string[]>(listData);
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) =>
     setValue(e.target.value);
+
+  useEffect(() => {
+    if (value) {
+      setData(
+        listData.filter((elem: string) =>
+          elem.toLowerCase().includes(value.toLowerCase())
+        )
+      );
+    } else {
+      setData(listData);
+    }
+  }, [value]);
 
   return (
     <div className={classes.app}>
       <header className={classes.header}>Test Application</header>
       <div className={classes.content}>
         <Search value={value} onChange={onChangeSearch} />
-        <List items={listData} />
+        <List items={data} />
       </div>
     </div>
   );
