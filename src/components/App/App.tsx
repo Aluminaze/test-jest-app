52c57b6e9ca8dbe13e7576f7cc1561d875eac056
Хегай Артем <khegay.art@gmail.com>
2021-09-16 15:12:34 +0300

    use Search component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./App.module.css";
 import List from "components/List";
 import { listData } from "TestData/ListData";
+import Search from "components/Search";
 
 function App() {
+  const [value, setValue] = useState<string>("");
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
+
   return (
     <div className={classes.app}>
       <header className={classes.header}>Test Application</header>
       <div className={classes.content}>
+        <Search value={value} onChange={onChangeSearch} />
         <List items={listData} />
       </div>
     </div>
