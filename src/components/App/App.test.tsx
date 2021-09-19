196d017dc3cbd825b1e0e764443e51de28ec57a0
Хегай Артем <khegay.art@gmail.com>
2021-09-19 10:24:55 +0300

    add test for App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/test application/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component:", () => {
+  it("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/test application/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("search work correctly", () => {
+    render(<App />);
+
+    const inputElem = screen.getByRole("textbox");
+    expect(inputElem).toBeInTheDocument();
+
+    userEvent.type(inputElem, "script");
+    expect(screen.queryByText(/javascript/i)).toBeInTheDocument();
+    expect(screen.queryByText(/typescript/i)).toBeInTheDocument();
+    expect(screen.queryByText(/SCSS/i)).not.toBeInTheDocument();
+  });
 });
