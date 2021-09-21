ecbc4ed511c00fb0f716edb33ec172a5f462485c
Хегай Артем <aluminaze@gmail.com>
2021-09-21 13:40:33 +0300

    update App component, add routes, update styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from "react";
 import classes from "./App.module.css";
-import List from "components/List";
-import { listData } from "TestData/ListData";
-import Search from "components/Search";
-import axios from "axios";
+
+import { Switch, Route, Link } from "react-router-dom";
+import Landing from "components/LandingPage";
+import SearchPage from "components/SearchPage";
+import CitiesPage from "components/CitiesPage";
 
 function getUserData() {
   return Promise.resolve(() => ({ id: 77, name: "Artem" }));
 }
 
-const GET_CITIES_URL =
-  "https://countriesnow.space/api/v0.1/countries/population/cities";
-
 function App() {
-  const [value, setValue] = useState<string>("");
-  const [data, setData] = useState<string[]>(listData);
-  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setValue(e.target.value);
   const [userData, setUserData] = useState<any | null>(null);
-  const [cities, setCities] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -29,47 +22,29 @@ function App() {
     fetchUserData();
   }, []);
 
-  useEffect(() => {
-    if (value) {
-      setData(
-        listData.filter((elem: string) =>
-          elem.toLowerCase().includes(value.toLowerCase())
-        )
-      );
-    } else {
-      setData(listData);
-    }
-  }, [value]);
-
-  const handleFetchCities = async () => {
-    const data = await axios
-      .get(GET_CITIES_URL)
-      .then((data) => data.data?.data);
-    const topCities = data?.length ? data.slice(0, 10) : [];
-    setCities(topCities);
-  };
-
   return (
     <div className={classes.app}>
       <header className={classes.header}>
-        <h1>Test Application</h1>
-        {userData && <span>Logged in as: {userData?.name}</span>}
+        <ul className={classes.nav}>
+          <li>
+            <Link to="/">Landing</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/cities">Cities</Link>
+          </li>
+        </ul>
+        <div>{userData && <span>User: {userData?.name}</span>}</div>
       </header>
+
       <div className={classes.content}>
-        <Search value={value} onChange={onChangeSearch} />
-        <List items={data} />
-
-        <div className={classes.cities}>
-          <button onClick={handleFetchCities}>Fetch cities</button>
-
-          <ul className={classes.citiesList} data-testid="cities">
-            {cities.map((city: any) => (
-              <li key={city.city}>
-                {city.city} ({city.country})
-              </li>
-            ))}
-          </ul>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/search" component={SearchPage} />
+          <Route exact path="/cities" component={CitiesPage} />
+        </Switch>
       </div>
     </div>
   );
