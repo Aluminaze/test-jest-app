e78b7e1c1323c57330fc9e033b5cf73dd9d58c16
Хегай Артем <aluminaze@gmail.com>
2021-09-22 10:19:33 +0300

    update LandingPage component

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import classes from "./LandingPage.module.css";
 
 const LandingPage = () => {
-  return <div>Landing page!</div>;
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.title}>Landing page!</h1>
+      <div className={classes.block}>Block</div>
+      <div className={classes.block}>Block</div>
+      <div className={classes.block}>Block</div>
+      <div className={classes.block}>Block</div>
+    </div>
+  );
 };
 
 export default LandingPage;
