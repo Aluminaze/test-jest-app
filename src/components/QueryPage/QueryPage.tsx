a9aeb6b1616ead218d2568319258dfd674486895
Хегай Артем <aluminaze@gmail.com>
2021-09-25 14:56:29 +0300

    get goods with goodsApi

--- src/components/QueryPage/QueryPage.tsx
+++ src/components/QueryPage/QueryPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import classes from "./styles.module.css";
+import { useGetGoodsQuery } from "redux-app";
 
 const QueryPage = () => {
+  const { data: goods = [], isLoading: isGoodsLoading } = useGetGoodsQuery();
+
+  if (isGoodsLoading) return <h2>Data is loading...</h2>;
+
   return (
     <div>
       <h1>Query Page</h1>
+      <ul>
+        {goods.map((item: any) => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
