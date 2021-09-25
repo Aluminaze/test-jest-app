cab9f23a5dabf8f75d1470edce71332d21464aea
Хегай Артем <aluminaze@gmail.com>
2021-09-25 15:42:57 +0300

    implement function to delete products

--- src/components/QueryPage/QueryPage.tsx
+++ src/components/QueryPage/QueryPage.tsx
@@ -1,10 +1,16 @@
 import React, { ChangeEvent, useState } from "react";
-import { useAddProductMutation, useGetGoodsQuery } from "redux-app";
+import {
+  useAddProductMutation,
+  useGetGoodsQuery,
+  useDeleteProductMutation,
+} from "redux-app";
+import classes from "./styles.module.css";
 
 const QueryPage = () => {
   const [productName, setProductName] = useState("");
   const { data: goods = [], isLoading: isGoodsLoading } = useGetGoodsQuery();
   const [addProduct] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
 
   if (isGoodsLoading) return <h2>Data is loading...</h2>;
 
@@ -12,6 +18,12 @@ const QueryPage = () => {
     if (productName) {
       await addProduct({ name: productName }).unwrap();
       setProductName("");
+    }
+  };
+
+  const handleDeleteProduct = async (id: number) => {
+    if (id) {
+      await deleteProduct(id);
     }
   };
 
@@ -31,11 +43,15 @@ const QueryPage = () => {
       >
         Add product
       </button>
-      <ul>
-        {goods.map((item: any) => (
-          <li key={item.name}>{item.name}</li>
-        ))}
-      </ul>
+      {Boolean(goods.length) && (
+        <ul className={classes.list}>
+          {goods.map((item: any) => (
+            <li onClick={() => handleDeleteProduct(item.id)} key={item.name}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
