6943c2c7ec3638d657fd49a635fe92277f4952ae
Хегай Артем <aluminaze@gmail.com>
2021-09-26 13:02:28 +0300

    display todos

--- src/components/TodoPage/TodoPage.tsx
+++ src/components/TodoPage/TodoPage.tsx
@@ -1,12 +1,39 @@
 import React from "react";
+import { useQuery } from "react-query";
 import classes from "./styles.module.css";
+import api from "api";
+
+interface TodoItem {
+  id: number;
+  title: string;
+  text: string;
+}
 
 const TodoPage = () => {
+  const { data: todoData = [], isLoading } = useQuery("todos", api.fetchTodos);
+
+  if (isLoading) {
+    return <h1>Loading data...</h1>;
+  }
+
   return (
     <div className={classes.container}>
-      <h1>TODO</h1>
+      <div className={classes.content}>
+        <h1>TODO LIST</h1>
 
-      <ul className={classes.list}></ul>
+        <div>
+          {Boolean(todoData.length) && (
+            <ul className={classes.list}>
+              {todoData.map((todo: TodoItem) => (
+                <li className={classes.listItem} key={todo.id}>
+                  <h2>{todo.title}</h2>
+                  <span>{todo.text}</span>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
