817f00f5baebaba4e320c7be8509893e9ae30aee
Хегай Артем <aluminaze@gmail.com>
2021-09-26 13:01:58 +0300

    wrap app with react query client

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,18 @@ import App from "components/App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "redux-app";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const app = (
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </QueryClientProvider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
